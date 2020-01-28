7b3cb02b2dadbdf9e97e31febfe0e33d28b77e78
Lauren Rose <larose@radford.edu>
2020-01-28 15:22:29 -0500

    some linting fixes

--- src/KeyPressWriter.java
+++ src/KeyPressWriter.java
@@ -1,9 +1,13 @@
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class KeyPressWriter {
 
-    private BufferedWriter writer;
-    private File file = new File("keyPresses.txt");;
+//    private BufferedWriter writer;
+    private File file = new File("keyPresses.txt");
+
+    private static Logger lggr = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     KeyPressWriter() {
 
@@ -14,19 +18,12 @@ class KeyPressWriter {
         if (newMap) {
             deleteFile();
         }
-        try {
-            writer = new BufferedWriter(new FileWriter(file, true));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
             writer.write(keyPressed);
             writer.newLine();
             writer.flush();
         } catch (IOException e) {
-            System.out.println("Error while writing to file.");
-        } finally {
-            if (writer != null) try {
-                writer.close();
-            } catch (IOException e) {
-                System.out.println("Something strange has happened");
-            }
+            lggr.log(Level.INFO, "Error while writing to file.");
         }
     }
 
